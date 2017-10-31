51a33da431160d784bb72e0782712aa4793d7240
smpr <gcbrown1116@gmail.com>
2017-10-31 09:22:18 -0400

    routes setup and show page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,9 +2,44 @@ require 'HTTParty'
 require 'pp'
 class MoviesController < ApplicationController
     before_action :authenticate_user!
-def index
-    @movies = Movie.all
+    def index
+        @movies = Movie.all
+    end 
 
-end
+    def show
+        @movie_id = params[:id]
+        @movie = Movie.find(@movie_id)
+    end
 
+    def new
+        @movie = Movie.new
+    end 
+
+    def create
+       @new_movie = Movie.new(movie_params) 
+       @new_movie.save
+       redirect_to movie_path(@new_movie)
+    end
+
+    def edit
+        @movie = Movie.find(params[:id])
+    end
+
+    def update
+        @movie = Movie.find(params[:id])
+        @movie.update(movie_params)
+        redirect_to movie_path(@movie)
+    end
+
+    def destroy
+        Movir.destroy(params[:id])
+        redirect_to movies_path
+    end
+
+    
+    private
+
+    def movir_params
+        params.require(:movie).permit(:title, :nationality, :photo_url)
+    end
 end
